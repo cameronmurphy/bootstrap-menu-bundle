7dfd7bc2be38296f92219a0c3623e1cb761de6fa
Cameron Murphy <camurphy@gmail.com>
2019-03-13 12:09:37 +1100

    WIP

---
+++ DependencyInjection/BootstrapMenuExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Camurphy\BootstrapMenuBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+class BootstrapMenuExtension extends Extension
+{
+    /**
+     * @param array $configs
+     * @param ContainerBuilder $container
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('bootstrap_menu.xml');
+
+        $configuration = $this->getConfiguration($configs, $container);
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $menus = [];
+    }
+}
--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Camurphy\BootstrapMenuBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -11,22 +12,65 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('bootstrap_menu');
+        $treeBuilder = new TreeBuilder('bootstrap_menu');
+        $rootNode = method_exists(TreeBuilder::class, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('swiftmailer');
 
         $rootNode
+            ->beforeNormalization()
+                ->ifNull()
+                ->thenEmptyArray()
+            ->end()
             ->children()
-            ->arrayNode('menu')
-            ->isRequired()
-            ->children()
-            ->scalarNode('resource')
-            ->isRequired()
-            ->cannotBeEmpty()
-            ->end()
-            ->end()
-            ->end()
+                ->append($this->getMenusNode())
             ->end();
 
         return $treeBuilder;
+    }
+
+    private function getMenusNode(): NodeDefinition
+    {
+        $treeBuilder = new TreeBuilder('menus');
+        $node = method_exists(TreeBuilder::class, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('mailers');
+
+        $node
+            ->requiresAtLeastOneElement()
+            ->useAttributeAsKey('name')
+            ->prototype('array')
+                ->children()
+                    ->scalarNode('label')->end()
+                    ->booleanNode('is_separator')->defaultFalse()->end()
+                    ->scalarNode('header_text')->end()
+                    ->scalarNode('route')->end()
+                    ->arrayNode('route_parameters')
+                        ->prototype('variable')
+                        ->end()
+                    ->end()
+                    ->scalarNode('uri')->end()
+                    ->booleanNode('display')->defaultTrue()->end()
+                    ->integerNode('order')->end()
+                    ->arrayNode('attributes')
+                        ->prototype('variable')
+                        ->end()
+                    ->end()
+                    ->arrayNode('link_attributes')
+                        ->prototype('variable')
+                        ->end()
+                    ->end()
+                    ->arrayNode('children_attributes')
+                        ->prototype('variable')
+                        ->end()
+                    ->end()
+                    ->arrayNode('label_attributes')
+                        ->prototype('variable')
+                        ->end()
+                    ->end()
+                    ->arrayNode('roles')
+                        ->prototype('scalar')
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        return $node;
     }
 }
