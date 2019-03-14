9ac9b5dfa57c7fb238988f335178763cad515de0
Cameron Murphy <camurphy@gmail.com>
2019-03-15 01:06:56 +1100

    Add placeholder test class

---
+++ Tests/Twig/Extension/MenuExtensionTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Camurphy\BootstrapMenuBundle\Tests\Twig\Extension;
+
+use PHPUnit\Framework\TestCase;
+
+class MenuExtensionTest extends TestCase
+{
+
+}
