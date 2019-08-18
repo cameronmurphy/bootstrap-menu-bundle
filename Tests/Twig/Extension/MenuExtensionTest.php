ecaaf042ea8708eb5a05601bf159c11f36a0fad8
Cameron Murphy <camurphy@gmail.com>
2019-08-18 22:23:50 +1000

    Completed tests

--- Tests/Twig/Extension/MenuExtensionTest.php
+++ Tests/Twig/Extension/MenuExtensionTest.php
@@ -26,18 +26,18 @@ class MenuExtensionTest extends TestCase
     private static $mockMenus = [
         'main' => [
             'items' => [
-                'test_drop_down_1' => [
-                    'label' => 'Test Drop Down 1',
+                'dropdown_menu_1' => [
+                    'label' => 'Dropdown Menu 1',
                     'items' => [
-                        'drop_down_item_1' => [
-                            'label' => 'Drop Down Item 1 (this should be pruned)',
-                            'route' => 'app_drop_down_1_route',
+                        'dropdown_item_1' => [
+                            'label' => 'Dropdown Item 1 (should be pruned due to permissions)',
+                            'route' => 'app_dropdown_1_route',
                             'roles' => ['ROLE_ADMINISTRATOR'],
                             'is_divider' => false,
                         ],
-                        'drop_down_item_2' => [
-                            'label' => 'Drop Down Item 2 (this should render a drop-down-item)',
-                            'route' => 'app_drop_down_2_route',
+                        'dropdown_item_2' => [
+                            'label' => 'Dropdown Item 2 (should render a dropdown-item)',
+                            'route' => 'app_dropdown_2_route',
                             'roles' => [],
                             'route_parameters' => ['test' => '123'],
                             'is_divider' => false,
@@ -45,18 +45,18 @@ class MenuExtensionTest extends TestCase
                     ],
                     'roles' => [],
                 ],
-                'test_drop_down_2' => [
-                    'label' => 'Test Drop Down 2',
+                'dropdown_menu_2' => [
+                    'label' => 'Dropdown Menu 2',
                     'items' => [
-                        'drop_down_item_3' => [
-                            'label' => 'Drop Down Item 3 (this should render a drop-down-item)',
-                            'route' => 'app_drop_down_item_3_route',
+                        'dropdown_item_3' => [
+                            'label' => 'Dropdown Item 3 (should render a dropdown-item because user has permission)',
+                            'route' => 'app_dropdown_item_3_route',
                             'roles' => ['ROLE_USER'],
                             'route_parameters' => [],
                             'is_divider' => false,
                         ],
-                        'drop_down_item_4' => [
-                            'label' => 'Drop Down Item 4 (should render a drop-down-item linking to Disney)',
+                        'dropdown_item_4' => [
+                            'label' => 'Dropdown Item 4 (should render a dropdown-item linking to Disney)',
                             'url' => 'https://disney.com',
                             'roles' => [],
                             'is_divider' => false,
@@ -69,32 +69,116 @@ class MenuExtensionTest extends TestCase
                     ],
                     'roles' => [],
                 ],
-                'test_drop_down_3' => [
-                    'label' => 'Test Drop Down 3 (this whole menu should be pruned due to permissions)',
+                'dropdown_menu_3' => [
+                    'label' => 'Dropdown Menu 3 (this whole menu should be pruned due to permissions)',
                     'items' => [
-                        'drop_down_item_5' => [
-                            'label' => 'Drop Down Item 5',
+                        'dropdown_item_5' => [
+                            'label' => 'Dropdown Item 5',
+                            'route' => 'app_dropdown_item_5_route',
                             'roles' => ['ROLE_ADMINISTRATOR'],
                             'is_divider' => false,
                         ],
-                        'drop_down_item_6' => [
-                            'label' => 'Drop Down Item 6',
+                        'dropdown_item_6' => [
+                            'label' => 'Dropdown Item 6',
+                            'route' => 'app_dropdown_item_6_route',
                             'roles' => ['ROLE_SUPER_USER'],
                             'is_divider' => false,
                         ],
-                        'drop_down_item_7' => [
-                            'label' => 'Drop Down Item 7',
+                        'dropdown_item_7' => [
+                            'label' => 'Dropdown Item 7',
+                            'route' => 'app_dropdown_item_7_route',
                             'roles' => ['ROLE_ADMINISTRATOR'],
                             'is_divider' => false,
                         ],
                     ],
                     'roles' => [],
                 ],
-                'test_link_1' => [
+                'link_1' => [
                     'label' => 'Link 1 (this should render a link)',
                     'route' => 'app_link_1_route',
                     'route_parameters' => [],
                     'roles' => [],
+                ],
+                'dropdown_menu_4' => [
+                    'label' => 'Dropdown Menu 4',
+                    'items' => [
+                        'dropdown_item_8' => [
+                            'label' => 'Dropdown Item 8',
+                            'route' => 'app_dropdown_item_8_route',
+                            'route_parameters' => [],
+                            'roles' => [],
+                            'is_divider' => false,
+                        ],
+                        'divider_2' => [
+                            'label' => 'Divider 2 (should render a dropdown-header)',
+                            'roles' => [],
+                            'is_divider' => true,
+                        ],
+                        'dropdown_item_9' => [
+                            'label' => 'Dropdown Item 9',
+                            'route' => 'app_dropdown_item_9_route',
+                            'route_parameters' => [],
+                            'roles' => [],
+                            'is_divider' => false,
+                        ],
+                        'divider_3' => [
+                            // Should render a dropdown-divider
+                            'roles' => [],
+                            'is_divider' => true,
+                        ],
+                        'dropdown_item_10' => [
+                            'label' => 'Dropdown Item 10',
+                            'route' => 'app_dropdown_item_10_route',
+                            'route_parameters' => [],
+                            'roles' => [],
+                            'is_divider' => false,
+                        ],
+                        'dropdown_item_11' => [
+                            'label' => 'Dropdown Item 11',
+                            'route' => 'app_dropdown_item_11_route',
+                            'route_parameters' => [],
+                            'roles' => [],
+                            'is_divider' => false,
+                        ],
+                        'divider_4' => [
+                            'label' => 'Divider 4 (should be pruned because Dropdown Item 12 will also pruned due to permissions)',
+                            'roles' => [],
+                            'is_divider' => true,
+                        ],
+                        'dropdown_item_12' => [
+                            'label' => 'Dropdown Item 12 (should be pruned due to permissions)',
+                            'route' => 'app_dropdown_item_12_route',
+                            'route_parameters' => [],
+                            'roles' => ['ROLE_ADMINISTRATOR'],
+                            'is_divider' => false,
+                        ],
+                        'divider_5' => [
+                            'label' => 'Divider 5',
+                            'roles' => [],
+                            'is_divider' => true,
+                        ],
+                        'dropdown_item_13' => [
+                            'label' => 'Dropdown Item 13',
+                            'route' => 'app_dropdown_item_13_route',
+                            'route_parameters' => [],
+                            'roles' => [],
+                            'is_divider' => false,
+                        ],
+                    ],
+                    'roles' => [],
+                ],
+                'dropdown_menu_5' => [
+                    'label' => 'Dropdown Menu 5 (should be pruned due to permissions)',
+                    'items' => [
+                        'dropdown_item_14' => [
+                            'label' => 'Dropdown Item 14',
+                            'route' => 'app_dropdown_item_14_route',
+                            'route_parameters' => [],
+                            'roles' => [],
+                            'is_divider' => false,
+                        ],
+                    ],
+                    'roles' => ['ROLE_ADMINISTRATOR'],
                 ],
             ],
         ],
