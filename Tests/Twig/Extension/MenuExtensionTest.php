94f477e2f0f0dffb626280ba71721586edd44288
Cameron Murphy <camurphy@gmail.com>
2019-08-18 10:08:19 +1000

    WIP tests

--- Tests/Twig/Extension/MenuExtensionTest.php
+++ Tests/Twig/Extension/MenuExtensionTest.php
@@ -7,8 +7,11 @@ namespace Camurphy\BootstrapMenuBundle\Tests\Twig\Extension;
 use Camurphy\BootstrapMenuBundle\Twig\Extension\MenuExtension;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Spatie\Snapshots\MatchesSnapshots;
 use Symfony\Bridge\Twig\Extension\SecurityExtension;
 use Twig\Environment as TwigEnvironment;
+use Twig\Loader\FilesystemLoader;
+use Twig\TwigFunction;
 
 /**
  * @internal
@@ -16,22 +19,91 @@ use Twig\Environment as TwigEnvironment;
  */
 class MenuExtensionTest extends TestCase
 {
+    use MatchesSnapshots;
+
+    private $rootPath;
+
     private static $mockMenus = [
         'main' => [
             'items' => [
-                'test_drop_down' => [
-                    'label' => 'Test Drop Down',
+                'test_drop_down_1' => [
+                    'label' => 'Test Drop Down 1',
                     'items' => [
-                        'first_drop_down_item' => [
-                            'label' => 'First Drop Down Item',
-                            'route' => 'app_first_drop_down_link',
+                        'drop_down_item_1' => [
+                            'label' => 'Drop Down Item 1 (this should be pruned)',
+                            'route' => 'app_drop_down_1_route',
                             'roles' => ['ROLE_ADMINISTRATOR'],
+                            'is_divider' => false,
+                        ],
+                        'drop_down_item_2' => [
+                            'label' => 'Drop Down Item 2 (this should render a drop-down-item)',
+                            'route' => 'app_drop_down_2_route',
+                            'roles' => [],
+                            'route_parameters' => ['test' => '123'],
+                            'is_divider' => false,
                         ],
                     ],
+                    'roles' => [],
+                ],
+                'test_drop_down_2' => [
+                    'label' => 'Test Drop Down 2',
+                    'items' => [
+                        'drop_down_item_3' => [
+                            'label' => 'Drop Down Item 3 (this should render a drop-down-item)',
+                            'route' => 'app_drop_down_item_3_route',
+                            'roles' => ['ROLE_USER'],
+                            'route_parameters' => [],
+                            'is_divider' => false,
+                        ],
+                        'drop_down_item_4' => [
+                            'label' => 'Drop Down Item 4 (should render a drop-down-item linking to Disney)',
+                            'url' => 'https://disney.com',
+                            'roles' => [],
+                            'is_divider' => false,
+                        ],
+                        'divider_1' => [
+                            'label' => 'Divider 1 (this should be pruned, there\'s nothing below it',
+                            'roles' => [],
+                            'is_divider' => true,
+                        ],
+                    ],
+                    'roles' => [],
+                ],
+                'test_drop_down_3' => [
+                    'label' => 'Test Drop Down 3 (this whole menu should be pruned due to permissions)',
+                    'items' => [
+                        'drop_down_item_5' => [
+                            'label' => 'Drop Down Item 5',
+                            'roles' => ['ROLE_ADMINISTRATOR'],
+                            'is_divider' => false,
+                        ],
+                        'drop_down_item_6' => [
+                            'label' => 'Drop Down Item 6',
+                            'roles' => ['ROLE_SUPER_USER'],
+                            'is_divider' => false,
+                        ],
+                        'drop_down_item_7' => [
+                            'label' => 'Drop Down Item 7',
+                            'roles' => ['ROLE_ADMINISTRATOR'],
+                            'is_divider' => false,
+                        ],
+                    ],
+                    'roles' => [],
+                ],
+                'test_link_1' => [
+                    'label' => 'Link 1 (this should render a link)',
+                    'route' => 'app_link_1_route',
+                    'route_parameters' => [],
+                    'roles' => [],
                 ],
             ],
         ],
     ];
+
+    public function setUp(): void
+    {
+        $this->rootPath = realpath(__DIR__ . '/../../../');
+    }
 
     /**
      * @throws \Twig\Error\LoaderError
@@ -45,16 +117,29 @@ class MenuExtensionTest extends TestCase
 
         $menuExtension = new MenuExtension(self::$mockMenus);
         $menu = $menuExtension->renderMenu($mockTwigEnvironment, 'main');
+
+        $this->assertMatchesSnapshot($menu);
     }
 
+    /**
+     * @throws \Twig\Error\LoaderError
+     *
+     * @return MockObject
+     */
     private function mockTwigEnvironment(): MockObject
     {
+        $loader = new FilesystemLoader([], $this->rootPath);
+        $loader->addPath('Resources/views', 'BootstrapMenu');
+
+        /** @var \Twig\Environment|MockObject $twigMock */
         $twigMock = $this->getMockBuilder(TwigEnvironment::class)
-            ->disableOriginalConstructor()
+            ->setConstructorArgs([$loader])
+            ->setMethods(['getExtension'])
             ->getMock();
 
         $securityExtensionMock = $this->getMockBuilder(SecurityExtension::class)
             ->disableOriginalConstructor()
+            ->setMethods(['isGranted', 'getDefaultStrategy'])
             ->getMock();
 
         $securityExtensionMock
@@ -67,6 +152,18 @@ class MenuExtensionTest extends TestCase
             ->expects($this->any())
             ->method('getExtension')
             ->willReturn($securityExtensionMock);
+
+        $pathFunction = new TwigFunction('path', function ($route, $routeParameters = []): string {
+            $path = '/' . str_replace('_', '-', $route);
+
+            if (\count($routeParameters) > 0) {
+                $path .= '?' . http_build_query($routeParameters);
+            }
+
+            return $path;
+        });
+
+        $twigMock->addFunction($pathFunction);
 
         return $twigMock;
     }
---
+++ Tests/Twig/Extension/__snapshots__/MenuExtensionTest__testRenderMenu__1.php
@@ -0,0 +1,17 @@
+<?php return '<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Test Drop Down 1
+    </a><div class="dropdown-menu"><a class="dropdown-item" href="/app-drop-down-2-route?test=123"
+>
+      Drop Down Item 2 (this should render a drop-down-item)
+    </a></div></li><li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Test Drop Down 2
+    </a><div class="dropdown-menu"><a class="dropdown-item" href="/app-drop-down-item-3-route"
+>
+      Drop Down Item 3 (this should render a drop-down-item)
+    </a><a class="dropdown-item" href="https://disney.com"
+>
+      Drop Down Item 4 (should render a drop-down-item linking to Disney)
+    </a></div></li><li class="nav-item"><a class="nav-link" href="/app-link-1-route"
+>
+      Link 1 (this should render a link)
+    </a></li>';
